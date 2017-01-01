580a399ab4227a2ecf10e0e88febb285ea7da109
Peter Velkov <kidroca@gmail.com>
2017-01-01 17:46:35 +0200

    refactor(login.component): replaced inputs with `app-form-input`

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -3,17 +3,17 @@ import {AppUserService} from '../../core/data/app-user.service';
 import {Router} from '@angular/router';
 import {NgForm} from '@angular/forms';
 import {AuthGuardService} from '../auth-guard.service';
+import {Submittable} from '../../shared/abstractions/submittable';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.less']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent extends Submittable implements OnInit {
 
     private redirectUrl: string;
 
-    message: string;
     username: string;
     password: string;
 
@@ -22,22 +22,19 @@ export class LoginComponent implements OnInit {
         private userService: AppUserService,
         private authGuard: AuthGuardService) {
 
-        this.message = 'Logged ' + (this.userService.authenticated() ? 'in' : 'out');
+        super();
+
         this.redirectUrl = this.authGuard.redirectUrl || '/home';
     }
 
     ngOnInit() {}
 
-    onSubmit(loginForm: NgForm) {
-
-        if (loginForm.invalid) {
-            return;
-        }
+    onValidForm(loginForm?: NgForm): Promise<boolean> {
 
         this.userService.setUsername(this.username);
         this.userService.setPassword(this.password);
 
-        this.userService.logIn()
+        return Promise.resolve<boolean>(this.userService.logIn())
             .then(res => {
                 console.log(res);
                 return this.router.navigate([this.redirectUrl]);
