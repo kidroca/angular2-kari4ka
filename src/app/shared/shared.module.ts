6e54e6f45a68b68b5019bd765b3a37d36cf17d5c
Peter Velkov <kidroca@gmail.com>
2017-01-02 21:27:28 +0200

    refactor(shared.module): registered ngbModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,15 +6,18 @@ import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { FormInputComponent } from './form-input/form-input.component';
 import {ErrorDefinitionService} from './form-input/error-definition.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     imports: [
         FormsModule,
-        CommonModule
+        CommonModule,
+        NgbModule
     ],
     exports: [
         FormsModule,
         CommonModule,
+        NgbModule,
         FormInputComponent
     ],
     declarations: [FormInputComponent],
