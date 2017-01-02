704fbfc0832b7cc06e96227c705986dd39071fa8
Peter Velkov <kidroca@gmail.com>
2017-01-02 21:26:59 +0200

    refactor(gallery.module): added shared module and declared file drop directive

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,21 +1,23 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {GalleryRoutingModule} from './gallery-routing.module';
 import {ImagesComponent} from './images/images.component';
 import {AddImageComponent} from './add-image/add-image.component';
 import {GalleryComponent} from './gallery.component';
 import { ThumbnailComponent } from './thumbnail/thumbnail.component';
+import { FileDropDirective } from './add-image/file-drop.directive';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     imports: [
-        CommonModule,
+        SharedModule,
         GalleryRoutingModule
     ],
     declarations: [
         GalleryComponent,
         ImagesComponent,
         AddImageComponent,
-        ThumbnailComponent
+        ThumbnailComponent,
+        FileDropDirective
     ]
 })
 export class GalleryModule {}
